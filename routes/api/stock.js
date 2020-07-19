64a50d7809dc431dee0b3e83a8f72a7e606dd069
ttcode10 <ttcode10@gmail.com>
2020-07-19 23:21:21 +0800

    Added stock profile creation route

--- routes/api/stock.js
+++ routes/api/stock.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const config = require('config');
 const request = require('request');
-const { response } = require('express');
+const { check, validationResult }  = require('express-validator');
 
+const Stock = require('./../../models/Stock');
 
 // @route    GET api/stock/:symbol
 // @desc     Get specific stock daily chart by symbol
@@ -37,22 +38,48 @@ router.get('/:symbol/chart', async (req, res) => {
 });
 
 
-// @route    GET api/stock/:symbol/votes
-// @desc     Get bullish & bearish votes on specific stock
+// @route    POST api/stock/:symbol
+// @desc     Create stock profile
 // @access   Public
-router.get('/:symbol/votes', async (req, res) => {
-  try {
-    request(options, (error, response, body) => {
-      if(error) console.log(error);
-      if(response.statusCode !== 200) {
-        return res.status(404).json({message: 'No data found'});
+router.post('/:symbol', [
+    check('symbol', 'Symbol is required').not().isEmpty(),
+  ], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({errors: errors.array()});
+    }
+
+    const symbol = req.params.symbol;
+
+    const stockField = {}
+    if(symbol) stockField.symbol = symbol;
+
+    try {
+      // See if the stock profile exist
+      let stock = await Stock.findOne({symbol});
+
+      // Return the stock profile if exists
+      if(stock) {
+        return res.json(stock);
       }
-      return res.json(JSON.parse(body));
-    });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).send('Server error');
-  }
+
+      // Create a new stock profile if not exists
+      const stockProfile = new Stock(stockField);
+      await stockProfile.save();
+      return res.json(stockProfile);
+
+
+      request(options, (error, response, body) => {
+        if(error) console.log(error);
+        if(response.statusCode !== 200) {
+          return res.status(404).json({message: 'No data found'});
+        }
+        return res.json(JSON.parse(body));
+      });
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).send('Server error');
+    }
 });
 
 
